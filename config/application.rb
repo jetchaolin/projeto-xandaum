82dc76b9ef02ccf46a28f9f319495cf1496169a9
Jetulio <joo_ramos2006@hotmail.com>
2024-12-05 12:30:36 -0300

    Altering application.rb not to require db and unit tests

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Escolha os frameworks que você quer carregar explicitamente:
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+# Se você não usar Active Storage, pode removê-lo também:
+# require "active_storage/engine"
+# require "sprockets/railtie" # Para assets (se usar sprockets)
+# require "rails/test_unit/railtie" # Opcional: para testes integrados
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
