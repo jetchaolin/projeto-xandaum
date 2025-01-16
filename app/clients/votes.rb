ec8e4525582cf3b14c8b2bfbfe45feef29c00e99
Jetulio <joo_ramos2006@hotmail.com>
2025-01-16 15:18:55 -0300

    added date selector to votes page

--- app/clients/votes.rb
+++ app/clients/votes.rb
@@ -10,4 +10,22 @@ class Votes
       JSON.parse(response.body)
     end
   end
+  def self.by_date(initial_date, final_date)
+    params = {
+      dataInicio: initial_date,
+      dataFim: final_date,
+      ordenarPor: "dataHoraInicio",
+      ordem: "DESC",
+    }.compact_blank
+
+    url = "https://dadosabertos.camara.leg.br/api/v2/votacoes"
+    headers = {
+      "Accept" => "application/json"
+    }
+
+    Rails.cache.fetch("filtered_votes", expires_in: 1.day) do
+      response = Faraday.get(url, params, headers)
+      JSON.parse(response.body)
+    end
+  end
 end
